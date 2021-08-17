a1e179a29b3f79fc080cac2a136869ca02e2de4a
kei-g <km.8k6ce+github@gmail.com>
2021-08-17 15:39:51 +0900

    :recycle: Define constant values to test
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- test.ts
+++ test.ts
@@ -2,17 +2,10 @@ import { PriorityQueue } from './priority-queue'
 
 const queue = new PriorityQueue<number>((lhs: number, rhs: number) => rhs - lhs)
 
-queue.add(3)
-queue.add(1)
-queue.add(4)
-queue.add(15)
-queue.add(9)
-queue.add(2)
-queue.add(6)
-queue.add(5)
-queue.add(35)
-queue.add(8)
-queue.add(97)
+const pi = [3, 1, 4, 15, 9, 2, 6, 5, 35, 8, 97]
+
+for (const value of pi)
+  queue.add(value)
 
 while (!queue.isEmpty) {
   const value = queue.pop()
